173495c8763488da8872a0559e3e556c8cc82666
upccup <yaoyunsmile@163.com>
2016-11-04 17:37:30 +0800

    removr sonsul

--- backend/add.go
+++ backend/add.go
@@ -10,11 +10,6 @@ func (b *Backend) RegisterApplication(application *types.Application) error {
 }
 
 // RegisterApplicationVersion register application version in consul.
-<<<<<<< d4131a5d2b159499b83ae9848fe54f1aa6b14d75
-func (b *Backend) RegisterApplicationVersion(appId string, version *types.Version) error {
-	return b.store.RegisterApplicationVersion(appId, version)
-=======
 func (b *Backend) RegisterApplicationVersion(appId string, version *types.ApplicationVersion) error {
 	return b.store.PutVersion(appId, version)
->>>>>>> update backend
 }
