4d5a137d8a819b7072f82626a99321d0bfb54cb2
mgniu <mgniu@dataman-inc.com>
2016-11-15 10:58:04 +0800

    fixed version manage bug

--- backend/add.go
+++ backend/add.go
@@ -2,6 +2,8 @@ package backend
 
 import (
 	"github.com/Dataman-Cloud/swan/types"
+	"reflect"
+	"sort"
 )
 
 // RegisterApplication register application in db.
@@ -11,5 +13,24 @@ func (b *Backend) SaveApplication(application *types.Application) error {
 
 // RegisterApplicationVersion register application version in db.
 func (b *Backend) SaveVersion(appId string, version *types.Version) error {
+	versions, err := b.store.ListVersions(appId)
+	if err != nil {
+		return err
+	}
+
+	if len(versions) != 0 {
+		sort.Strings(versions)
+
+		newestVersion, err := b.store.FetchVersion(versions[len(versions)-1])
+		if err != nil {
+			return err
+		}
+
+		if reflect.DeepEqual(version, newestVersion) {
+			return nil
+		}
+
+	}
+
 	return b.store.SaveVersion(version)
 }
