88c41888c2f7d82e758979765e36beb48e39b2f6
kevin xu <cming.xu@gmail.com>
2016-12-08 09:06:16 +0800

    remove rollback instances
    Signed-off-by: kevin xu <cming.xu@gmail.com>

--- src/manager/framework/api/types.go
+++ src/manager/framework/api/types.go
@@ -5,19 +5,18 @@ import (
 )
 
 type App struct {
-	ID                string    `json:"id,omitempty"`
-	Name              string    `json:"name,omitempty"`
-	Instances         int       `json:"instances,omitempty"`
-	UpdatedInstances  int       `json:"updatedInstances,omitempty"`
-	RunningInstances  int       `json:"runningInstances"`
-	RollbackInstances int       `json:"rollbackInstances,omitempty"`
-	RunAs             string    `json:"runAs,omitempty"`
-	ClusterId         string    `json:"clusterId,omitempty"`
-	Status            string    `json:"status,omitempty"`
-	Created           time.Time `json:"created,omitempty"`
-	Updated           time.Time `json:"updated,omitempty"`
-	Mode              string    `json:"mode,omitempty"`
-	State             string    `json:"state,omitempty"`
+	ID               string    `json:"id,omitempty"`
+	Name             string    `json:"name,omitempty"`
+	Instances        int       `json:"instances,omitempty"`
+	UpdatedInstances int       `json:"updatedInstances,omitempty"`
+	RunningInstances int       `json:"runningInstances"`
+	RunAs            string    `json:"runAs,omitempty"`
+	ClusterId        string    `json:"clusterId,omitempty"`
+	Status           string    `json:"status,omitempty"`
+	Created          time.Time `json:"created,omitempty"`
+	Updated          time.Time `json:"updated,omitempty"`
+	Mode             string    `json:"mode,omitempty"`
+	State            string    `json:"state,omitempty"`
 
 	// use task for compatability now, should be slot here
 	Tasks    []*Task  `json:"tasks,omitempty"`
--- src/manager/framework/state/app.go
+++ src/manager/framework/state/app.go
@@ -170,18 +170,6 @@ func (app *App) Delete() error {
 	return nil
 }
 
-func (app *App) SetState(state string) {
-	app.InvalidateCallbacks = make(map[string][]AppInvalidateCallbackFuncs)
-	logrus.Infof("now clearing all InvalidateCallbacks")
-
-	app.State = state
-	logrus.Infof("app %s now has state %s", app.AppId, app.State)
-}
-
-func (app *App) StateIs(state string) bool {
-	return app.State == state
-}
-
 func (app *App) Update(version *types.Version) error {
 	if app.ProposedVersion != nil {
 		return errors.New("previous rolling update in progress")
@@ -264,6 +252,18 @@ func (app *App) CancelUpdate() error {
 	return nil
 }
 
+func (app *App) SetState(state string) {
+	app.InvalidateCallbacks = make(map[string][]AppInvalidateCallbackFuncs)
+	logrus.Infof("now clearing all InvalidateCallbacks")
+
+	app.State = state
+	logrus.Infof("app %s now has state %s", app.AppId, app.State)
+}
+
+func (app *App) StateIs(state string) bool {
+	return app.State == state
+}
+
 // called when slot has any update
 func (app *App) InvalidateSlots() {
 	// handle callback
@@ -331,10 +331,6 @@ func (app *App) MarkForDeletionInstances() int {
 	}
 
 	return markForDeletionInstances
-}
-
-func (app *App) RollbackInstances() int {
-	return 0
 }
 
 func (app *App) CanBeCleanAfterDeletion() bool {
