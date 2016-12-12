ac7ccf7307ae55db3e7ba5f4127ffa0a9f5dc777
upccup <yaoyunsmile@163.com>
2016-12-12 15:30:38 +0800

    make cli build success

--- src/types/application.go
+++ src/types/application.go
@@ -1,5 +1,7 @@
 package types
 
+// UNMAINTAINED: DO NOT IMPORT THIS PKG IN NEW FILE
+
 type Version struct {
 	ID           string
 	AppId        string
@@ -83,4 +85,51 @@ type HealthCheck struct {
 
 type Command struct {
 	Value string
+}
+
+type Task struct {
+	ID             string
+	Name           string
+	Command        string
+	Cpus           float64
+	Disk           float64
+	Mem            float64
+	Image          string
+	Network        string
+	PortMappings   []*PortMappings
+	Privileged     bool
+	Parameters     []*Parameter
+	ForcePullImage bool
+	Volumes        []*Volume
+	Env            map[string]string
+	Labels         map[string]string
+	HealthChecks   []*HealthCheck
+	OfferId        string
+	AgentId        string
+	AgentHostname  string
+	Status         string
+	AppId          string
+	KillPolicy     *KillPolicy
+	Uris           []string
+}
+
+type PortMappings struct {
+	Port     uint32
+	Protocol string
+	Name     string
+}
+
+type Application struct {
+	ID                string
+	Name              string
+	Instances         uint64
+	UpdatedInstances  uint64
+	RunningInstances  uint64
+	RollbackInstances uint64
+	RunAs             string
+	ClusterId         string
+	Status            string
+	Created           int64
+	Updated           int64
+	Mode              string
 }
