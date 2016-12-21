c8c2e052ff078e010c8b4afa64f5c9016a0111e4
Chu Xiangyang <xychu2008@gmail.com>
2016-12-21 14:52:52 +0800

    Fix api-tests (#151)

--- api-test/app-create.sh
+++ api-test/app-create.sh
@@ -4,31 +4,31 @@
 # should be empty before first app created
 msg "apps list should be empty"
 apps=`http --check-status --ignore-stdin --timeout=4.5 get $SERVER_PATH/$PATH_PREFIX/apps`
-appsLen=`echo ${apps} | jq '.apps | length'`
+appsLen=`echo ${apps} | jq 'length'`
 
 if [ "$appsLen" == "0" ]
 then
   ok "there is no apps running now"
 else
   msg "apps list not empty as expected"
-  for app in `echo ${apps} | jq '.apps[].name' | tr -d '"'`; do
+  for app in `echo ${apps} | jq '.[].name' | tr -d '"'`; do
     http --check-status --ignore-stdin --timeout=4.5 delete $SERVER_PATH/$PATH_PREFIX/apps/${app} &>/dev/null
   done
 fi
 
 msg "create app with malformat json should fail"
 http --check-status --ignore-stdin --timeout=4.5 post $SERVER_PATH/$PATH_PREFIX/apps foobar=xbz  &>/dev/null
-if [ "$?" != "4" ]
+if [ $? == 0 ]
 then
   fail "create app with malformatted json should fail"
 else
-  ok "create app with same name should fail"
+  ok "create app with malformatted json failed"
 fi
 
 # create a application
 msg "create replicates mode application now"
 http --check-status --ignore-stdin --timeout=4.5 post $SERVER_PATH/$PATH_PREFIX/apps @template-replicates.json  &>/dev/null
-if [ "$?" != "4" ]
+if [ $? == 0 ]
 then
   ok "app successfully created"
 else
@@ -38,7 +38,7 @@ fi
 
 # should have one app running
 msg "should have one app running now"
-appsLen=`http --check-status --ignore-stdin --timeout=4.5 get $SERVER_PATH/$PATH_PREFIX/apps | jq '.apps | length'`
+appsLen=`http --check-status --ignore-stdin --timeout=4.5 get $SERVER_PATH/$PATH_PREFIX/apps | jq 'length'`
 
 if [ "$appsLen" == "1" ]
 then
@@ -48,10 +48,10 @@ fi
 # create duplicated application should faild
 msg "create application now with same name should fail"
 http --check-status --ignore-stdin --timeout=4.5 post $SERVER_PATH/$PATH_PREFIX/apps @template-replicates.json  &>/dev/null
-if [ "$?" != "4" ]
+if [ $? == 0 ]
 then
-  fail "create app with same name `cat @template-replicates.json | jq .name` should fail"
+  fail "create app with same name `cat template-replicates.json | jq .appId` should fail"
 else
-  ok "create app with same name should fail"
+  ok "create app with same name failed"
 fi
 
