3eef991ba6cdcbac6c82e206e2a7fbc237a2fa3b
Ethan Chu <xychu2008@gmail.com>
2016-12-21 12:55:39 +0800

    Enable log filter for API server

--- src/manager/new_apiserver/server.go
+++ src/manager/new_apiserver/server.go
@@ -3,6 +3,8 @@ package new_apiserver
 import (
 	"net/http"
 	"path/filepath"
+	"strings"
+	"time"
 
 	"github.com/Sirupsen/logrus"
 	"github.com/emicklei/go-restful"
@@ -47,6 +49,9 @@ func (apiServer *ApiServer) Start() error {
 		Container:      wsContainer}
 	wsContainer.Filter(cors.Filter)
 
+	// Add log filter
+	wsContainer.Filter(NCSACommonLogFormatLogger())
+
 	// Optionally, you can install the Swagger Service which provides a nice Web UI on your REST API
 	// You need to download the Swagger HTML5 assets and change the FilePath location in the config below.
 	// Open http://localhost:8080/apidocs and enter http://localhost:8080/apidocs.json in the api input field.
@@ -70,4 +75,26 @@ func (apiServer *ApiServer) Start() error {
 	logrus.Fatal(server.ListenAndServe())
 
 	return nil
+}
+
+func NCSACommonLogFormatLogger() restful.FilterFunction {
+	return func(req *restful.Request, resp *restful.Response, chain *restful.FilterChain) {
+		var username = "-"
+		if req.Request.URL.User != nil {
+			if name := req.Request.URL.User.Username(); name != "" {
+				username = name
+			}
+		}
+		chain.ProcessFilter(req, resp)
+		logrus.Printf("%s - %s [%s] \"%s %s %s\" %d %d",
+			strings.Split(req.Request.RemoteAddr, ":")[0],
+			username,
+			time.Now().Format("02/Jan/2006:15:04:05 -0700"),
+			req.Request.Method,
+			req.Request.URL.RequestURI(),
+			req.Request.Proto,
+			resp.StatusCode(),
+			resp.ContentLength(),
+		)
+	}
 }
