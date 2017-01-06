7ec7c90d32f996914ea4d1879920c2d8cff05b55
my9074 <313231068@qq.com>
2017-01-06 14:57:40 +0800

    Add cluster/user for app monitor parameter

--- frontend/src/app/conf.js
+++ frontend/src/app/conf.js
@@ -1,4 +1,5 @@
 BACKEND_URL_BASE = {
-    defaultBase: "http://localhost:5015",
-    logProxyBase: ""
+  defaultBase: "http://localhost:5015",
+  logBase: "", // example: http://localhost:8000/ui/logbase/logWithoutKey
+  monitorBase: "" // example: http://localhost:8000/ui/monitor/chart
 };
