ff6a3cab8146136dce69c318eec4285ba71e081d
kevin xu <cming.xu@gmail.com>
2017-01-20 15:06:33 +0800

    pass api test
    Signed-off-by: kevin xu <cming.xu@gmail.com>

--- api-test/app-rolling-update.sh
+++ api-test/app-rolling-update.sh
@@ -5,9 +5,9 @@ http --check-status --ignore-stdin --timeout=4.5 put $SERVER_PATH/$PATH_PREFIX/a
 
 wait_a_moment # wait scalling up operation done
 
-task0Version=`http get http://localhost:9999/v_beta/apps/nginx0051  | jq '.tasks | .[] | select( .id | contains("0-")) .versionId'`
-task1Version=`http get http://localhost:9999/v_beta/apps/nginx0051  | jq '.tasks | .[] | select( .id | contains("1-")) .versionId'`
-appVersion=`http get http://localhost:9999/v_beta/apps/nginx0051  | jq '.versions | .[]'`
+task0Version=`http get http://localhost:9999/v_beta/apps/$APP_NAME  | jq '.tasks | .[] | select( .id | contains("0-")) .versionId'`
+task1Version=`http get http://localhost:9999/v_beta/apps/$APP_NAME  | jq '.tasks | .[] | select( .id | contains("1-")) .versionId'`
+appVersion=`http get http://localhost:9999/v_beta/apps/$APP_NAME  | jq '.currentVersion.id'`
 
 if [ "$task0Version" != "$appVersion" ]
 then
