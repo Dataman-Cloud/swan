c44da0dba2ce6e2557226426afed8cf96c8d8296
mgniu <mgniu@dataman-inc.com>
2017-02-13 11:01:12 +0800

    move `frontend` to separate project `swan-ui`

--- frontend/src/app/conf.js
+++
@@ -1,5 +0,0 @@
-BACKEND_URL_BASE = {
-  defaultBase: "http://localhost:5015",
-  logBase: "", // example: http://localhost:8000/ui/logbase/logWithoutKey
-  monitorBase: "" // example: http://localhost:8000/ui/monitor/chart
-};
