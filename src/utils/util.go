eb9a502251d90b9e969adda487d61457f9af9ef5
upccup <yaoyunsmile@163.com>
2017-03-09 17:31:44 +0800

    fix bug unmarshal event failed in agent

--- src/utils/util.go
+++ src/utils/util.go
@@ -1,9 +1,13 @@
 package utils
 
 import (
+	"encoding/json"
 	"errors"
+	"fmt"
 	"io/ioutil"
 	"os"
+	"reflect"
+	"strings"
 
 	"github.com/Sirupsen/logrus"
 	"github.com/coreos/etcd/pkg/fileutil"
@@ -44,4 +48,53 @@ func CreateNodeID(filePath string) (string, error) {
 	logrus.Infof("starting swan node, ID file was not found started with  new ID: %s", nodeID)
 
 	return nodeID, nil
+}
+
+func SetField(obj interface{}, name string, value interface{}) error {
+	structValue := reflect.ValueOf(obj).Elem()
+	structFieldValue := structValue.FieldByName(name)
+	notFoundValue := reflect.Value{}
+	if structFieldValue == notFoundValue {
+		structFieldValue = structValue.FieldByNameFunc(
+			func(fname string) bool {
+				return strings.ToLower(name) == strings.ToLower(fname)
+			})
+	}
+
+	if !structFieldValue.IsValid() {
+		return fmt.Errorf("No such field: %s in obj", name)
+	}
+
+	if !structFieldValue.CanSet() {
+		return fmt.Errorf("Cannot set %s field value", name)
+	}
+
+	structFieldType := structFieldValue.Type()
+	val := reflect.ValueOf(value)
+	if structFieldType != val.Type() {
+		number, ok := value.(json.Number)
+		if ok && structFieldType.Kind() == reflect.Uint32 {
+			intValue, err := number.Int64()
+			if err != nil {
+				return err
+			}
+			structFieldValue.Set(reflect.ValueOf(uint32(intValue)))
+			return nil
+		}
+
+		return errors.New("Provided value type didn't match obj field type")
+	}
+
+	structFieldValue.Set(val)
+	return nil
+}
+
+func FillStruct(s interface{}, m map[string]interface{}) error {
+	for k, v := range m {
+		err := SetField(s, k, v)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
