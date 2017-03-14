7e87177423d7e31839c04a7f8f670d85adb0f70e
kevin xu <cming.xu@gmail.com>
2017-03-14 16:47:27 +0800

    reorgnized file name
    Signed-off-by: kevin xu <cming.xu@gmail.com>

--- src/manager/framework/state/app.go
+++ src/manager/framework/state/app.go
@@ -60,6 +60,12 @@ type App struct {
 	UserEventChan chan *event.UserEvent
 }
 
+type AppsByUpdated []*App
+
+func (a AppsByUpdated) Len() int           { return len(a) }
+func (a AppsByUpdated) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a AppsByUpdated) Less(i, j int) bool { return a[i].Updated.After(a[j].Updated) } // NOTE(xychu): Desc order
+
 func NewApp(version *types.Version,
 	userEventChan chan *event.UserEvent) (*App, error) {
 	appID := fmt.Sprintf("%s-%s-%s", version.AppName, version.RunAs, connector.Instance().ClusterID)
--- src/manager/framework/state/slot.go
+++ src/manager/framework/state/slot.go
@@ -76,6 +76,12 @@ type Slot struct {
 	touched       bool
 }
 
+type SlotsById []*Slot
+
+func (a SlotsById) Len() int           { return len(a) }
+func (a SlotsById) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a SlotsById) Less(i, j int) bool { return a[i].Index < a[j].Index }
+
 type SlotResource struct {
 	CPU  float64
 	Mem  float64
