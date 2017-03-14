cce50f6478aea997522948e4c33bc0ea6c0a9817
kevin xu <cming.xu@gmail.com>
2017-03-14 15:15:49 +0800

    remove unused code
    Signed-off-by: kevin xu <cming.xu@gmail.com>

--- src/manager/framework/state/slot.go
+++ src/manager/framework/state/slot.go
@@ -298,7 +298,6 @@ func (slot *Slot) SetState(state string) error {
 	// skip app invalidation if slot state is not mesos driven
 	if (slot.State != SLOT_STATE_PENDING_OFFER) ||
 		(slot.State != SLOT_STATE_PENDING_KILL) {
-		//slot.App.Reevaluate()
 		slot.App.Step()
 	}
 
