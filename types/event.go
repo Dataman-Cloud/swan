2818f6dc5f8bcd07566e028fcf6c91a703309443
bbklab <bbklab@126.com>
2017-06-26 19:22:17 +0800

    refactor nameserver

--- types/event.go
+++ types/event.go
@@ -24,64 +24,8 @@ type TaskEvent struct {
 	GatewayEnabled bool    `json:"gateway"`
 }
 
-type SSEEvent struct {
-	name    string
-	payload *TaskEvent
-}
-
+// Format format task events to SSE text
 func (e *TaskEvent) Format() []byte {
-
-	ev := &SSEEvent{
-		name:    e.Type,
-		payload: e,
-	}
-
-	bs, err := json.Marshal(ev)
-	if err != nil {
-		fmt.Printf("marshal event got error: %v", err)
-
-		return []byte("")
-	}
-
-	return bs
-	//e := &eventbus.Event{
-	//	Type:    eventType,
-	//	AppID:   slot.App.ID,
-	//	AppMode: string(slot.App.Mode),
-	//}
-
-	//gatewayEnabled := true
-	//if slot.Version.Gateway != nil {
-	//	gatewayEnabled = slot.Version.Gateway.Enabled
-	//}
-	//payload := &types.TaskInfoEvent{
-	//	TaskID:         slot.ID,
-	//	AppID:          slot.App.ID,
-	//	VersionID:      slot.Version.ID,
-	//	AppVersion:     slot.Version.AppVersion,
-	//	State:          slot.State,
-	//	Healthy:        slot.healthy,
-	//	ClusterID:      slot.App.ClusterID,
-	//	RunAs:          slot.Version.RunAs,
-	//	Weight:         slot.weight,
-	//	AppName:        slot.App.Name,
-	//	SlotIndex:      slot.Index,
-	//	GatewayEnabled: gatewayEnabled,
-	//}
-
-	//if slot.App.IsFixed() {
-	//	payload.IP = slot.Ip
-	//	payload.Mode = string(APP_MODE_FIXED)
-	//} else {
-	//	payload.IP = slot.AgentHostName
-	//	payload.Mode = string(APP_MODE_REPLICATES)
-	//	if len(slot.CurrentTask.HostPorts) > 0 {
-	//		payload.Port = uint32(slot.CurrentTask.HostPorts[0])
-	//		payload.PortName = slot.Version.Container.Docker.PortMappings[0].Name
-	//	}
-	//}
-
-	//e.Payload = payload
-
-	//return e
+	bs, _ := json.Marshal(e)
+	return []byte(fmt.Sprintf("event: %s\ndata: %s\n\n", e.Type, string(bs)))
 }
