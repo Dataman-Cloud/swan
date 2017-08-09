83bc5ffff5843cede3b187dca5515b19e6b8239f
pwzgorilla <pwzgorilla@gmail.com>
2017-08-09 20:55:44 +0800

    tiny improvement

--- utils/weight.go
+++ utils/weight.go
@@ -6,11 +6,11 @@ import (
 
 func ComputeWeight(n, t, c float64) float64 {
 	x := (t - n) * 100 * c
-	y := n * (c - 1)
+	y := n * (1 - c)
 
 	if y == 0 || x == 0 {
 		return 100
 	}
 
-	return math.Floor(-x/y + 0.5)
+	return math.Floor(x/y + 0.5)
 }
