7e4334512a4cbd236cde6bd7e0e145908faf1d9a
pwzgorilla <pwzgorilla@gmail.com>
2017-08-16 17:02:37 +0800

    more ci test for canary update

--- integration-test/swan_api_canary_test.go
+++ integration-test/swan_api_canary_test.go
@@ -87,6 +87,55 @@ func (s *ApiSuite) TestCanaryUpdate(c *check.C) {
 	c.Assert(counter[vers[0].ID], check.Equals, 3)
 	c.Assert(counter[vers[1].ID], check.Equals, 2)
 
+	// canary continue
+	body = &types.CanaryUpdateBody{
+		Instances: 5,
+		Value:     0.5,
+		OnFailure: "continue",
+		Delay:     0.5,
+	}
+	s.canaryUpdate(id, body, c)
+	err = s.waitApp(id, types.OpStatusNoop, time.Second*180, c)
+	c.Assert(err, check.IsNil)
+	fmt.Println("TestCanaryUpdate() continued")
+
+	// verify app
+	app = s.inspectApp(id, c)
+	c.Assert(app.Name, check.Equals, "demo")
+	c.Assert(app.VersionCount, check.Equals, 2)
+	c.Assert(len(app.Version), check.Equals, 1)
+	c.Assert(app.OpStatus, check.Equals, types.OpStatusNoop)
+
+	// verify app tasks
+	tasks = s.listAppTasks(id, c)
+	c.Assert(len(tasks), check.Equals, 5)
+
+	var y int
+	for _, task := range tasks {
+		if task.Weight == 100 {
+			y++
+		}
+	}
+
+	c.Assert(y, check.Equals, 5)
+
+	// verify app versions
+	vers = s.listAppVersions(id, c)
+	c.Assert(len(vers), check.Equals, 2)
+	c.Assert(vers[0].Mem, check.Equals, float64(10))
+
+	counter = make(map[string]int)
+	for _, task := range tasks {
+		if v, ok := counter[task.Version]; ok {
+			v++
+			counter[task.Version] = v
+		} else {
+			counter[task.Version] = 1
+		}
+	}
+
+	c.Assert(counter[vers[0].ID], check.Equals, 5)
+
 	// clean up
 
 	err = s.removeApp(id, time.Second*10, c)
